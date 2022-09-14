f8404c3d1fa79155d9c135f59bb1a41d09083a20
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-09-14 10:49:47 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,17 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [currentCategory, setCategory] = useState('All')
+  const [taskList, setTaskList] = useState(TASKS)
+
+  
+  console.log('rendering...')
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter currentCategory={currentCategory} setCategory={setCategory} categories={CATEGORIES} />
+      <NewTaskForm categories={CATEGORIES} setTaskList={setTaskList} taskList={taskList} />
+      <TaskList currentCategory={currentCategory} taskList={taskList} setTaskList={setTaskList} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ currentCategory, setCategory, categories }) {
+
+  function onCatButtonClick(event){
+    setCategory(event.target.value)
+  }
+
+  const catButtons = categories.map((category) => {
+    if(category === currentCategory){
+      return <button className="selected" value={category} onClick={onCatButtonClick} key={category}>{category}</button>
+    }
+    else{
+      return <button value={category} onClick={onCatButtonClick} key={category}>{category}</button>
+    }
+
+  });
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {catButtons}
     </div>
   );
 }
